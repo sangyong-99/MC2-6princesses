5dc66a6f7d07fd51af884de7ff26033654cf273f
jaelyung <61307199+jaelyung@users.noreply.github.com>
2023-05-09 00:05:51 +0900

    Revert "[INT] Onboarding 1&3 Pages"

--- sixprincesses/ContentView.swift
+++ sixprincesses/ContentView.swift
@@ -8,21 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @StateObject var userInformation : UserInformation = UserInformation()
-    @StateObject var viewController : ViewController = ViewController()
-    
     var body: some View {
         VStack {
-            
-            switch (viewController.currentPage) {
-            case 1 : View_1(userInformation: userInformation, viewController : viewController)
-            case 2 : View_3(userInformation: userInformation, viewController : viewController)
-            default:
-                Text("error")
-            }
-            
-            
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
         }
         .padding()
     }
