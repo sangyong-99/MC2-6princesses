3a68782e4fcafacd2ab945a1e27aed4b7b1fe521
‘sangyong-99’ <tkddyd1126@gmail.com>
2023-05-08 11:37:33 +0900

    ModelData

---
+++ sixprincesses/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  sixprincesses
+//
+//  Created by 신상용 on 2023/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
